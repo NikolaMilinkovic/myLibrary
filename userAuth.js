79fff4b22435abc156b40171e8265def3c50be37
Nikola Milinkovic <nikolamilinkovic221@gmail.com>
2023-12-29 00:43:08 +0100

    Test fixes

--- userAuth.js
+++ userAuth.js
@@ -1,5 +1,5 @@
-// import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
-// import { getAuth, GoogleAuthProvider, signInWithPopup, onAuthStateChanged } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
+import { getAuth, GoogleAuthProvider, signInWithPopup, onAuthStateChanged } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
 
 
 //     // Your web app's Firebase configuration
@@ -102,9 +102,6 @@
 
 
 
-
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
-import { getAuth, GoogleAuthProvider, signInWithPopup, onAuthStateChanged } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
 
 document.addEventListener('DOMContentLoaded', function() {
     // Your web app's Firebase configuration
